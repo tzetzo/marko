8457f28f89b1bf4c90e24334b3cf2a1a869f776c
kefche1 <kefche1@abv.bg>
2019-01-29 10:57:35 +0200

    chnaged./img, ./gltf & news.json to ./img_desktop/mobile, ./gltf_desktop/mobile & news_mobile/desktop.json

--- js/main.js
+++ js/main.js
@@ -4,6 +4,14 @@ if ( WEBGL.isWebGLAvailable() === false ) { document.body.appendChild( WEBGL.get
 //set the hash to home:
 history.pushState ? history.pushState(null, null, '#home') : location.hash = '#home';
 
+//check if mobile or desktop device is used:
+function mobileAndTabletcheck() {
+    var check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
+};
+mobileAndTabletcheck()? (device = 'mobile') : (device = 'desktop');
+alert(device);
 let mixer,
     link = '#home',
     INTERSECTED,
@@ -38,11 +46,11 @@ const webGLcontainer = document.getElementById( 'webGLcontainer' );
 const magnifyWebGLcontainer = document.querySelector('.magnify__glass');
 const newsContainer = document.querySelector('.news');
 
-const gold = new THREE.TextureLoader().load("./img/gold_roughness.jpg", (map) => {});  //  './' for altervista
-const wood = new THREE.TextureLoader().load("./img/wood_roughness.jpg");
-const sofia = new THREE.TextureLoader().load("./models/gltf/sofia.jpg");
-const book = new THREE.TextureLoader().load("./img/bookCover_roughness.jpg");
-const booksN = new THREE.TextureLoader().load("./img/booksN.jpg");
+const gold = new THREE.TextureLoader().load(`./img_${device}/gold_roughness.jpg`, (map) => {});  //  './' for altervista
+const wood = new THREE.TextureLoader().load(`./img_${device}/wood_roughness.jpg`);
+const sofia = new THREE.TextureLoader().load(`./img_${device}/sofia.jpg`);
+const book = new THREE.TextureLoader().load(`./img_${device}/bookCover_roughness.jpg`);
+const booksN = new THREE.TextureLoader().load(`./img_${device}/booksN.jpg`);
 book.wrapS = THREE.MirroredRepeatWrapping;  //horizontal
 book.wrapT = THREE.MirroredRepeatWrapping;  //THREE.RepeatWrapping, THREE.MirroredRepeatWrapping, THREE.ClampToEdgeWrapping
 book.repeat.set( 40, 1 ); //40,1
@@ -250,7 +258,7 @@ function init() {
 
       			scene.video = document.createElement( 'video' );
       			scene.video.loop = true;
-      			scene.video.src = './img/tvStatic.mp4';
+      			scene.video.src = `./img_${device}/tvStatic.mp4`;
             scene.video.muted = 'muted';
             //make E.P. show TV static signal
             tvModel.getObjectByName('tvEPscreen').material.map = new THREE.VideoTexture( scene.video );
@@ -305,7 +313,7 @@ function init() {
 
 function loadGltf(gltfLoader, model) {
     return new Promise((resolve,reject) => {
-        gltfLoader.load( `./models/gltf/${model}.gltf`, (gltf) => {
+        gltfLoader.load( `./models/gltf_${device}/${model}.gltf`, (gltf) => {
                 resolve(gltf);
             }, undefined, (e) => {
                 reject(e);
@@ -1172,7 +1180,7 @@ function googleMapInit() {
         title: 'Markovski Lawyers LLP',
         draggable: false,
         animation: google.maps.Animation.BOUNCE,  //BOUNCE, DROP
-        icon: './img/icoSmall.png'
+        icon: `./img_${device}/icoSmall.png`
     });
 }
 
@@ -1298,7 +1306,7 @@ function getFlameMaterial() {
 }
 
 function newsLoad() {
-    fetch('./js/news.json', {
+    fetch(`./js/news_${device}.json`, {
             method: "GET",
             mode: "cors", // no-cors, cors, *same-origin
             cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
@@ -1382,7 +1390,7 @@ function newsMouseMove( e ) {
 function newsCloudsLoad() {
     geometry = new THREE.Geometry();
 
-    var texture = new THREE.TextureLoader().load("./img/cloud.png");
+    var texture = new THREE.TextureLoader().load(`./img_${device}/cloud.png`);
     texture.magFilter = THREE.LinearFilter;
     texture.minFilter = THREE.LinearFilter;
 
@@ -1454,11 +1462,11 @@ function muteUnmute () {
     if (!scene.music.isMuted() || !scene.musicMenu.isMuted()){
         scene.music.mute();
         scene.musicMenu.mute();
-        document.querySelector('.volume-icon').setAttribute('href', "./img/sprite.svg#volume-x");
+        document.querySelector('.volume-icon').setAttribute('href', `./img_${device}/sprite.svg#volume-x`);
     } else {
         scene.musicMenu.unmute();
         scene.music.unmute();
-        document.querySelector('.volume-icon').setAttribute('href', "./img/sprite.svg#volume");
+        document.querySelector('.volume-icon').setAttribute('href', `./img_${device}/sprite.svg#volume`);
     }
 }
 
